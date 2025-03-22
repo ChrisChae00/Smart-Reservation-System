fb02c957b9d8c942ca45b55269b0f1a761e90eab
ChrisChae00 <chrischae00@gmail.com>
2025-03-21 21:26:04 -0400

    Add project files.

---
+++ DineReserve/Controllers/ReservationsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DineReserve.Models;
+
+namespace DineReserve.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save to database (future)
+                return RedirectToAction("Confirmation", reservation);
+            }
+            return View(reservation);
+        }
+
+        public IActionResult Confirmation(Reservation reservation)
+        {
+            return View(reservation);
+        }
+    }
+}
---
+++ DineReserve/Models/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DineReserve.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime ReservationTime { get; set; }
+
+        [Range(1, 20)]
+        public int NumberOfGuests { get; set; }
+    }
+}
